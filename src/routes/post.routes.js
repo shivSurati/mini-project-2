4a3aab08c8db00d2bfcff343b97ffb4953bbd198
shivam surati <shivamsurati04@gmail.com>
2026-04-04 15:54:49 +0530

    completed authentication cycle process project

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,9 +1,10 @@
 //DUMMY API!!!!
 const express = require("express");
 const router = express.Router();
+const userModel = require("../models/user.model");
 const jwt = require("jsonwebtoken");
 
-router.post("/create", (req, res) => {
+router.post("/create", async (req, res) => {
   console.log(req.body);
   console.log(req.cookies);
   const token = req.cookies.token;
@@ -14,7 +15,10 @@ router.post("/create", (req, res) => {
   }
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log("decoded");
+    const user = await userModel.findOne({
+      _id: decoded.id,
+    });
+    console.log(user);
   } catch (error) {
     res.status(401).json({
       message: "Token is invalid!",
